c8a46151d336f86d6f8c07ff8a595f3088b38c08
Dmytrii-Pylypchuk <dimapilipchuk@knu.ua>
2024-09-19 22:50:40 +0300

    Add files via upload

---
+++ ipzz/ipzz.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n;
+    char choice;
+
+    do {
+        cout << "Введіть кількість елементів: ";
+        cin >> n;
+
+        cout << "Числа Фібоначчі: ";
+        for (int i = 0; i < n; i++) {
+            cout << fibonacci(i) << " ";
+        }
+        cout << endl;
+
+        cout << "Бажаєте повторити? (y/n): ";
+        cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y');
+
+    return 0;
+}
